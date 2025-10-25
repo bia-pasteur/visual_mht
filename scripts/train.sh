68936051ae8ade9a854483b425d29dbaec1ecfc8
Raphael Reme <raphaelreme@protonmail.com>
2025-10-25 19:52:29 +0200

    Update training exp name

--- scripts/train.sh
+++ scripts/train.sh
@@ -12,10 +12,10 @@ expyrun configs/train/sinetra.yml --training.epochs 300
 # expyrun configs/train/sinetra.yml --training.epochs 300 --model.backbone PreActResNet20
 
 # Let's see the impact of pseudo tracking augmentation
-expyrun configs/train/trasein.yml --training.epochs 300 --dataset.delta_t 0
-expyrun configs/train/sinetra.yml --training.epochs 300 --dataset.delta_t 0
+expyrun configs/train/trasein.yml --training.epochs 300 --dataset.delta_t 0 --__run__.__name__ trasein_no_track
+expyrun configs/train/sinetra.yml --training.epochs 300 --dataset.delta_t 0 --__run__.__name__ sinetra_no_track
 
 
 # Let's see the impact of random augmentation
-expyrun configs/train/trasein.yml --training.epochs 300 --dataset.elastic.prob 0.0 --dataset.affine.prob 0.0 -dataset.erase.prob 0.0 --dataset.motion_blur.prob 0.0 --dataset.blur_shot_noise 0.0 --dataset.jitter 0.0
-expyrun configs/train/sinetra.yml --training.epochs 300 --dataset.elastic.prob 0.0 --dataset.affine.prob 0.0 -dataset.erase.prob 0.0 --dataset.motion_blur.prob 0.0 --dataset.blur_shot_noise 0.0 --dataset.jitter 0.0
+expyrun configs/train/trasein.yml --training.epochs 300 --dataset.elastic.prob 0.0 --dataset.affine.prob 0.0 --dataset.erase.prob 0.0 --dataset.motion_blur.prob 0.0 --dataset.blur_shot_noise.prob 0.0 --dataset.jitter.prob 0.0  --__run__.__name__ trasein_no_aug
+expyrun configs/train/sinetra.yml --training.epochs 300 --dataset.elastic.prob 0.0 --dataset.affine.prob 0.0 --dataset.erase.prob 0.0 --dataset.motion_blur.prob 0.0 --dataset.blur_shot_noise.prob 0.0 --dataset.jitter.prob 0.0 --__run__.__name__ sinetra_no_aug
